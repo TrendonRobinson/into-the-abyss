82b4b09629f2a90d781ff741090efd81149b0409
John Nguyen <johnhoangn@gmail.com>
2022-02-06 19:31:12 -0500

    add combat types

--- src/Shared/Enums/CombatRequestType.lua
+++ src/Shared/Enums/CombatRequestType.lua
@@ -1,4 +1,9 @@
 return {
     MeleeRequest = 0;
-    MeleeHitRequest = 1;
+    MeleeHitRequest = 2;
+
+    ReplicateArcane = 10;
+    ReplicateArchery = 11;
+    ReplicateMelee = 12;
+    ReplicateSkill = 13;
 }
